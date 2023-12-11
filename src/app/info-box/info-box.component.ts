b00787fdb6b8f9cf6cefb4b2bbbdca74b4cab60d
Selman Ciftci <selman.ciftci.coding@gmail.com>
2023-12-11 11:23:23 +0100

    dynamic style works

--- src/app/info-box/info-box.component.ts
+++ src/app/info-box/info-box.component.ts
@@ -1,13 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 
 @Component({
 	selector: 'app-info-box',
 	standalone: true,
-	imports: [],
+	imports: [CommonModule],
 	templateUrl: './info-box.component.html',
 	styleUrl: './info-box.component.scss',
 })
 export class InfoBoxComponent {
 	@Input() title = '(title)';
 	@Input() body = '(body)';
+	@Input() status = '';
+	statusColor = 'yellow';
+
+	ngOnInit() {
+		this.statusColor = this.status === 'success' ? 'green' : 'red';
+	}
 }
