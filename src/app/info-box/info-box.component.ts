01d7f5c4760a6270d61e074f69bb06cd317711ce
Selman Ciftci <selman.ciftci.coding@gmail.com>
2023-12-11 11:50:04 +0100

    ng class works

--- src/app/info-box/info-box.component.ts
+++ src/app/info-box/info-box.component.ts
@@ -13,10 +13,12 @@ export class InfoBoxComponent {
 	@Input() body = '(body)';
 	@Input() author = '(author)';
 	@Input() status = '(status unknown)';
+	@Input() highlight = '';
 
 	dateOfInfo = 'Dec 23, 2022';
 	statusColor = '';
 	showDevInfo = false;
+	highlighted = true;
 
 	ngOnInit() {
 		this.statusColor =
@@ -25,6 +27,7 @@ export class InfoBoxComponent {
 				: this.status === 'danger'
 				? 'tomato'
 				: 'gray';
+		this.highlighted = this.highlight === 'true';
 	}
 
 	handleShowDevInfoToggle = () => {
